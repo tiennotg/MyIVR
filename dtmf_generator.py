4838944cfa114c9d65002bfae76215b7223bda00
Guilhem Tiennot <guilhem@tiennotblog.fr>
2021-05-08 00:59:58 +0200

    Update dtmf_generator.py

--- dtmf_generator.py
+++ dtmf_generator.py
@@ -7,20 +7,20 @@
   
   Dependencies: sys, (tty, termios for Unix-like), numpy, pyaudio
   
-  This file is part of My202Modem.
+  This file is part of MyIVR.
   
-  My202Modem is free software: you can redistribute it and/or modify
+  MyIVR is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
   
-  My202Modem is distributed in the hope that it will be useful,
+  MyIVR is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
   
   You should have received a copy of the GNU General Public License
-  along with My202Modem.  If not, see <https://www.gnu.org/licenses/>.
+  along with MyIVR.  If not, see <https://www.gnu.org/licenses/>.
 '''
 
 import sys
